2f75276fc4e26eef8b7e66e8e6ce518888a7d80c
Kushal <kartikg396@gmail.com>
2020-10-18 00:04:30 +0530

    backend: updated announcement and post model and routes

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const CommentSchema = require("./Comment");
 const Schema = mongoose.Schema;
 
 // Create Schema
@@ -11,16 +12,24 @@ const PostSchema = new Schema({
     type: String,
     required: true
   },
-  createrType: {
-    type: String,
-    required: true
-  },
   creterId: {
     type: Schema.Types.ObjectId,
     required: true
   },
+  createrName: {
+    type: String,
+    required: true
+  },
+  createrType: {
+    type: String,
+    required: true
+  },
   link: String,
-  comments: [ String ]
+  comments: CommentSchema,
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("posts", PostSchema);
